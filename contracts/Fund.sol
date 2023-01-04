afaedfdcbcc90bfcdecc47114c8d9ddf0cf89ffb
Atharv Gulati <95168968+Atharv-02@users.noreply.github.com>
2023-01-04 08:17:48 +0530

    Update Fund.sol

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -26,7 +26,7 @@ contract Fund {
 
     function fund() public payable {
         require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] = msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
     
